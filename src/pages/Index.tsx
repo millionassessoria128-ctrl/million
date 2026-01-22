4c0f0a3969be4bbc94236d533177bfa176ae6721
FranciscoRoges09 <rogesfrancisco695@gmail.com>
2026-01-21 22:29:11 -0300

    Atualizando código

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,9 +1,9 @@
 import Header from '@/components/Header';
 import HeroSection from '@/components/HeroSection';
 import ComparisonSection from '@/components/ComparisonSection';
-import TestimonialsSection from '@/components/TestimonialsSection';
+
 import AboutSection from '@/components/AboutSection';
-import LeadFormSection from '@/components/LeadFormSection';
+
 import ClientsSection from '@/components/ClientsSection';
 import Footer from '@/components/Footer';
 
